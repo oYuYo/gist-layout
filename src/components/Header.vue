a8301645aa5eae28d236cf625046c0c4f367b05f
oYuYo <year0120@gmail.com>
2023-09-25 22:47:51 +0900

    up

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header>
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="https://github.com/oYuYo" target="_blank" rel="noopener noreferrer">Github</a></li>
+                    <li><a href="https://gist.github.com/oYuYo" target="_blank" rel="noopener noreferrer">Gists</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+<style scoped>
+nav {
+    text-align:left;
+    background-color: #000;
+    color:white;
+}
+nav ul li {
+    display: inline-block;
+    padding:0.8rem;
+}
+nav li:hover{
+    background-color: #87D7FF;
+}
+a {
+    color:white;
+}
+</style>
