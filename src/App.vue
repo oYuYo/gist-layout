48635199941f66caf60f706f2cf229ac33984e16
oYuYo <year0120@gmail.com>
2024-12-08 08:44:35 +0000

    up

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+import GistItem from './components/GistItem.vue'
+</script>
+
+<template>
+  <Header />
+
+  <main>
+    <div class="wrapper">
+      <div class="head">
+        <h1>Gist Items</h1>
+      </div>
+      <Suspense>
+        <template #default>
+          <GistItem />
+        </template>
+        <template #fallback>
+          Loading...
+        </template>
+    </Suspense>
+    </div>
+  </main>
+</template>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+h1 {
+  margin:1rem 0 0.1rem 0;
+}
+main {
+  max-width: 1280px;
+  min-width: 375px;
+  margin: 0 auto;
+}
+div.head {
+  border-bottom: 1px solid #cccccc;  
+}
+/*
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+*/
+</style>
---
+++ src/components/GistItem.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const gists = await fetch("https://api.github.com/users/oYuYo/gists", {
+  method: "GET",
+  headers: { 
+    Accept: "application/vnd.github+json",
+    Authorization: "Bearer: import.meta.env.GH_TOKEN",
+    "X-GitHub-Api-Version": "2022-11-28"
+  }
+})
+.then((response) => response.json());
+</script>
+<template>
+    <a v-for="item in gists" v-bind:href="item.html_url" target="_blank" rel="noopener noreferrer">
+    <div class="item">
+        <div class="gist-item">
+            <span class="description">{{item.description}}</span>
+            <span class="date">{{new Date(item.created_at).toLocaleDateString()}}</span>
+        </div>
+    </div>
+    </a>
+</template>
+
+<style scoped>
+.item {
+    display:flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    font-size:16px;
+    border-left: solid 2rem #333;
+    border-radius: 10px;
+    width:100%;
+    background-color:#F2F2F2;
+    color:black;
+    overflow: hidden;
+    margin:1.5rem 0 0 0;
+}
+.gist-item {
+    padding:2rem 0 2rem 1rem;
+    white-space: nowrap;
+}
+.date{
+    padding-left: 1rem;
+}
+</style>
